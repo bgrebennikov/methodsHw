82428c38950ba16197da01368127ce5c64c0c3b4
Boris <bgrebennikovv@gmail.com>
2025-01-15 13:20:32 +0400

    bank app

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -1,5 +1,8 @@
 package com.github.bgrebennikovv;
 
+import java.time.LocalDate;
+
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,10 +10,18 @@ public class Main {
 
         int year = 2024;
         if (isLeapYear(year)) {
-            System.out.printf("%s год - високосный год", year);
+            System.out.printf("%s год - високосный год\n", year);
         } else {
-            System.out.printf("%s год - невисокосный год", year);
+            System.out.printf("%s год - невисокосный год\n", year);
         }
+
+        // Task 2
+
+        int clientOS = 1; // 0 — iOS, 1 — Android
+        int clientDeviceYear = 2014;
+
+        suggestAppVersion(clientOS, clientDeviceYear);
+
     }
 
     public static boolean isLeapYear(int year) {
@@ -18,5 +29,22 @@ public class Main {
     }
 
 
+    public static void suggestAppVersion(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear(); // Получение текущего года
+
+        if (clientDeviceYear < currentYear) {
+            if (clientDeviceYear < 2015) {
+                System.out.println(clientOS == 0
+                        ? "Установите облегченную версию приложения для iOS по ссылке"
+                        : "Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println(clientOS == 0
+                        ? "Установите обычную версию приложения для iOS по ссылке"
+                        : "Установите обычную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Ваше устройство актуально, установите обычное приложение.");
+        }
+    }
 
 }
