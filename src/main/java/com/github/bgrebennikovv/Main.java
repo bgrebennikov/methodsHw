e9340a910d99f86eaa1634cb9a4281da5f628935
Boris <bgrebennikovv@gmail.com>
2025-01-15 13:14:10 +0400

    leap year

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -3,5 +3,20 @@ package com.github.bgrebennikovv;
 public class Main {
     public static void main(String[] args) {
 
+        // Task 1
+
+        int year = 2024;
+        if (isLeapYear(year)) {
+            System.out.printf("%s год - високосный год", year);
+        } else {
+            System.out.printf("%s год - невисокосный год", year);
+        }
     }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+
+
 }
